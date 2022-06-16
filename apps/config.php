aacfc120ef669ea0a53e91da4da6246639afefdc
MarthenTen <tenouyemarten@gmail.com>
2022-06-16 19:21:59 +0900

    final day07 project 01

---
+++ apps/config.php
@@ -0,0 +1,2 @@
+<?php
+$baseURL = 'http://127.0.0.1/php1-day08-project01/';
---
+++ views/404.php
@@ -0,0 +1,16 @@
+<div class="col-9">
+    <!-- .card -->
+    <div class="card">
+        <div class="card-header">
+            <h1>Error 401</h1>
+        </div>
+        <div class="card-body">
+            <p>Welcome to SaCode's Weekend!</p>
+        </div>
+
+        <div class="card-footer">
+            <p>Maaf Halman Tidak Ditemukan</p>
+        </div>
+    </div>
+    <!-- end .card -->
+</div>
